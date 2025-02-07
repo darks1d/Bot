4c8529c5c523a617d094d8bd41aa84c4bc75e6b5
vanko285 <kolr87lop@gmail.com>
2025-02-07 21:01:20 +0400

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -16,7 +16,6 @@ def text_handler(message):
   bot.send_message(chad_id'')
  else:
   bot.send_message(chad_id'')
-  
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 def start(update: Update, context: CallbackContext) -> None:update.message.reply_text('Привет! Я ваш бот.')
